fa89ec6df076bf31d02d4a2fd8e659d698497d20
Sean Lunsford <sean@seanlunsford.com>
2024-01-27 23:17:15 +0300

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Breadcrumbs",
+	"url": "https://breadcrumbs.fm/",
+	"language": "en",
+	"github_url": "https://github.com/slunsford/breadcrumbs.fm",
+	"author": {
+		"name": "Elias Lanz & Sean Lunsford",
+		"email": "breadcrumbs@breadcrumbs.fm",
+		"url": "https://breadcrumbs.fm/",
+	}
+}
